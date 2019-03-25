e56a697cf0515c9ecef25ec1ba8a31c23abbd6db
dimitry.kamarnikov <dimitry.kamarnikov@ironsrc.com>
2019-03-25 22:00:20 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-bind:msg="status"/>
+    <datetime placeholder="When to remind" type="datetime" v-model="date"></datetime>
+    <textarea v-model="message" placeholder="What to remind"></textarea>
+    <div>
+      <button id="submit" v-on:click="submitForm">Add</button>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      status: 'Add a reminder',
+      message: '',
+      date: moment().add('hour', 1).format(),
+    }
+  },
+  methods: {
+    submitForm: function(e) {
+      const apiUrl = window.AppConfig.API_URL
+      const chatId = window.AppConfig.CHAT_ID
+
+      if (!apiUrl || !chatId) {
+        console.error('API_URL and CHAT_ID are mandatory')
+        return
+      }
+
+      fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          chat: chatId,
+          message: this.message,
+          date: this.date
+        })
+      })
+      .then(r => r.json())
+      .then(res => {
+        this.$set(this, 'message', '')
+        this.$set(this, 'date', moment().add('hour', 1).format())
+        this.status = 'Successfully added'
+      })
+      .catch( e => console.log(e))
+    }
+  },
   components: {
     HelloWorld
   }
@@ -17,6 +60,19 @@ export default {
 </script>
 
 <style>
+body {margin: 0;}
+#submit {
+  border: 3px solid #2c3e50;
+  border-radius: 5px;
+  background: none;
+  font-size: 25px;
+  padding: 10px 20px;
+  outline: none;
+  cursor: pointer;
+}
+#submit:hover {
+  background-color: #42b983;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +80,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.vdatetime-input,
+#app textarea {
+  margin-bottom: 25px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 25px;
+  padding: 15px 25px;
+  width: 350px;
+  max-width: 100%;
+  color: #2c3e50;
+  border: 3px solid #2c3e50;
+  border-radius: 5px;
+  outline: none;
 }
 </style>
